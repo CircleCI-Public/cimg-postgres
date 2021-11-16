b91d36d9156d94117b9a42189b5d99bbce3d5a86
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-11-15 23:26:55 -0500

    Revert "Align compile opts w/ Debian pkg [release] (#20)" (#22)
    This reverts commit b71066b5dac0a34411e533a0602a42d83be3db80.

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 9.6/Dockerfile -t cimg/postgres:9.6.23  -t cimg/postgres:9.6 .
-docker build --file 9.6/postgis/Dockerfile -t cimg/postgres:9.6.23-postgis  -t cimg/postgres:9.6-postgis .
 docker build --file 13.4/Dockerfile -t cimg/postgres:13.4 .
 docker build --file 13.4/postgis/Dockerfile -t cimg/postgres:13.4-postgis .
