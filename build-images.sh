712fd5588494c070f0d301d81ebb75c6a2aa824e
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-12-07 21:01:03 +0000

    Pub: 9.6.24,10.19,11.4,12.9, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-docker build --file 9.6/Dockerfile -t cimg/postgres:9.6.23  -t cimg/postgres:9.6 .
-docker build --file 9.6/postgis/Dockerfile -t cimg/postgres:9.6.23-postgis  -t cimg/postgres:9.6-postgis .
+docker build --file 9.6/Dockerfile -t cimg/postgres:9.6.24  -t cimg/postgres:9.6 .
+docker build --file 9.6/postgis/Dockerfile -t cimg/postgres:9.6.24-postgis  -t cimg/postgres:9.6-postgis .
+docker build --file 10.19/Dockerfile -t cimg/postgres:10.19 .
+docker build --file 10.19/postgis/Dockerfile -t cimg/postgres:10.19-postgis .
+docker build --file 11.4/Dockerfile -t cimg/postgres:11.4 .
+docker build --file 11.4/postgis/Dockerfile -t cimg/postgres:11.4-postgis .
+docker build --file 12.9/Dockerfile -t cimg/postgres:12.9 .
+docker build --file 12.9/postgis/Dockerfile -t cimg/postgres:12.9-postgis .
+docker build --file 13.5/Dockerfile -t cimg/postgres:13.5 .
+docker build --file 13.5/postgis/Dockerfile -t cimg/postgres:13.5-postgis .
+docker build --file 14.1/Dockerfile -t cimg/postgres:14.1 .
+docker build --file 14.1/postgis/Dockerfile -t cimg/postgres:14.1-postgis .
--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-docker push cimg/postgres:9.6.23
+docker push cimg/postgres:9.6.24
 docker push cimg/postgres:9.6
-docker push cimg/postgres:9.6.23-postgis
+docker push cimg/postgres:9.6.24-postgis
 docker push cimg/postgres:9.6-postgis
+
+docker push cimg/postgres:10.19
+docker push cimg/postgres:10.19-postgis
+
+docker push cimg/postgres:11.4
+docker push cimg/postgres:11.4-postgis
+
+docker push cimg/postgres:12.9
+docker push cimg/postgres:12.9-postgis
+
+docker push cimg/postgres:13.5
+docker push cimg/postgres:13.5-postgis
+
+docker push cimg/postgres:14.1
+docker push cimg/postgres:14.1-postgis
