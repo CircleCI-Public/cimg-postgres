1bda9f14b1c14cd0eee11c8cba26fe9856745ad9
Nirojan Vythilingam <nirojan@users.noreply.github.com>
2021-12-15 14:05:43 +0000

    Add major version tags to latest major.minor Postgres release

--- build-images.sh
+++ build-images.sh
@@ -2,13 +2,13 @@
 
 docker build --file 9.6/Dockerfile -t cimg/postgres:9.6.24  -t cimg/postgres:9.6 .
 docker build --file 9.6/postgis/Dockerfile -t cimg/postgres:9.6.24-postgis  -t cimg/postgres:9.6-postgis .
-docker build --file 10.19/Dockerfile -t cimg/postgres:10.19 .
-docker build --file 10.19/postgis/Dockerfile -t cimg/postgres:10.19-postgis .
-docker build --file 11.4/Dockerfile -t cimg/postgres:11.4 .
-docker build --file 11.4/postgis/Dockerfile -t cimg/postgres:11.4-postgis .
-docker build --file 12.9/Dockerfile -t cimg/postgres:12.9 .
-docker build --file 12.9/postgis/Dockerfile -t cimg/postgres:12.9-postgis .
-docker build --file 13.5/Dockerfile -t cimg/postgres:13.5 .
-docker build --file 13.5/postgis/Dockerfile -t cimg/postgres:13.5-postgis .
-docker build --file 14.1/Dockerfile -t cimg/postgres:14.1 .
-docker build --file 14.1/postgis/Dockerfile -t cimg/postgres:14.1-postgis .
+docker build --file 10.19/Dockerfile -t cimg/postgres:10.19 -t cimg/postgres:10 .
+docker build --file 10.19/postgis/Dockerfile -t cimg/postgres:10.19-postgis -t cimg/postgres:10-postgis .
+docker build --file 11.4/Dockerfile -t cimg/postgres:11.4 -t cimg/postgres:11 .
+docker build --file 11.4/postgis/Dockerfile -t cimg/postgres:11.4-postgis -t cimg/postgres:11-postgis .
+docker build --file 12.9/Dockerfile -t cimg/postgres:12.9 -t cimg/postgres:12 .
+docker build --file 12.9/postgis/Dockerfile -t cimg/postgres:12.9-postgis -t cimg/postgres:12-postgis .
+docker build --file 13.5/Dockerfile -t cimg/postgres:13.5 -t cimg/postgres:13 .
+docker build --file 13.5/postgis/Dockerfile -t cimg/postgres:13.5-postgis -t cimg/postgres:13-postgis .
+docker build --file 14.1/Dockerfile -t cimg/postgres:14.1 -t cimg/postgres:14 .
+docker build --file 14.1/postgis/Dockerfile -t cimg/postgres:14.1-postgis -t cimg/postgres:14-postgis .
--- push-images.sh
+++ push-images.sh
@@ -6,16 +6,26 @@ docker push cimg/postgres:9.6.24-postgis
 docker push cimg/postgres:9.6-postgis
 
 docker push cimg/postgres:10.19
+docker push cimg/postgres:10
 docker push cimg/postgres:10.19-postgis
+docker push cimg/postgres:10-postgis
 
 docker push cimg/postgres:11.4
+docker push cimg/postgres:11
 docker push cimg/postgres:11.4-postgis
+docker push cimg/postgres:11-postgis
 
 docker push cimg/postgres:12.9
+docker push cimg/postgres:12
 docker push cimg/postgres:12.9-postgis
+docker push cimg/postgres:12-postgis
 
 docker push cimg/postgres:13.5
+docker push cimg/postgres:13
 docker push cimg/postgres:13.5-postgis
+docker push cimg/postgres:13-postgis
 
 docker push cimg/postgres:14.1
+docker push cimg/postgres:14
 docker push cimg/postgres:14.1-postgis
+docker push cimg/postgres:14-postgis
