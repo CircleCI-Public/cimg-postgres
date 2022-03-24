488ae46cac6e4ce74ae592f4aa8490cee4553cf3
Jalexchen <jeff.chen@circleci.com>
2022-03-24 11:34:31 -0700

    Publish v11.14 and v12.10. [release] (#43)

--- build-images.sh
+++ build-images.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 9.6/Dockerfile -t cimg/postgres:9.6.24  -t cimg/postgres:9.6 .
-docker build --file 9.6/postgis/Dockerfile -t cimg/postgres:9.6.24-postgis  -t cimg/postgres:9.6-postgis .
-docker build --file 10.19/Dockerfile -t cimg/postgres:10.19 .
-docker build --file 10.19/postgis/Dockerfile -t cimg/postgres:10.19-postgis .
-docker build --file 11.4/Dockerfile -t cimg/postgres:11.4 .
-docker build --file 11.4/postgis/Dockerfile -t cimg/postgres:11.4-postgis .
-docker build --file 12.9/Dockerfile -t cimg/postgres:12.9 .
-docker build --file 12.9/postgis/Dockerfile -t cimg/postgres:12.9-postgis .
-docker build --file 13.5/Dockerfile -t cimg/postgres:13.5 .
-docker build --file 13.5/postgis/Dockerfile -t cimg/postgres:13.5-postgis .
-docker build --file 14.1/Dockerfile -t cimg/postgres:14.1 .
-docker build --file 14.1/postgis/Dockerfile -t cimg/postgres:14.1-postgis .
+docker build --file 11.14/Dockerfile -t cimg/postgres:11.14 .
+docker build --file 11.14/postgis/Dockerfile -t cimg/postgres:11.14-postgis .
+docker build --file 12.10/Dockerfile -t cimg/postgres:12.10 .
+docker build --file 12.10/postgis/Dockerfile -t cimg/postgres:12.10-postgis .
--- push-images.sh
+++ push-images.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/postgres:9.6.24
-docker push cimg/postgres:9.6
-docker push cimg/postgres:9.6.24-postgis
-docker push cimg/postgres:9.6-postgis
+docker push cimg/postgres:11.14
+docker push cimg/postgres:11.14-postgis
 
-docker push cimg/postgres:10.19
-docker push cimg/postgres:10.19-postgis
-
-docker push cimg/postgres:11.4
-docker push cimg/postgres:11.4-postgis
-
-docker push cimg/postgres:12.9
-docker push cimg/postgres:12.9-postgis
-
-docker push cimg/postgres:13.5
-docker push cimg/postgres:13.5-postgis
-
-docker push cimg/postgres:14.1
-docker push cimg/postgres:14.1-postgis
+docker push cimg/postgres:12.10
+docker push cimg/postgres:12.10-postgis
