71d8d480bd8ad8d25d84bab366006391d95b88af
Stuart Chinery <stuart.chinery@gmail.com>
2022-03-27 08:41:18 +0100

    Added 10.20, 11.15, 13.6 and 14.2 images

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 11.14/Dockerfile -t cimg/postgres:11.14 .
-docker build --file 11.14/postgis/Dockerfile -t cimg/postgres:11.14-postgis .
-docker build --file 12.10/Dockerfile -t cimg/postgres:12.10 .
-docker build --file 12.10/postgis/Dockerfile -t cimg/postgres:12.10-postgis .
+docker build --file 10.20/Dockerfile -t cimg/postgres:10.20 .
+docker build --file 10.20/postgis/Dockerfile -t cimg/postgres:10.20-postgis .
+docker build --file 11.15/Dockerfile -t cimg/postgres:11.15 .
+docker build --file 11.15/postgis/Dockerfile -t cimg/postgres:11.15-postgis .
+docker build --file 13.6/Dockerfile -t cimg/postgres:13.6 .
+docker build --file 13.6/postgis/Dockerfile -t cimg/postgres:13.6-postgis .
+docker build --file 14.2/Dockerfile -t cimg/postgres:14.2 .
+docker build --file 14.2/postgis/Dockerfile -t cimg/postgres:14.2-postgis .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/postgres:11.14
-docker push cimg/postgres:11.14-postgis
+docker push cimg/postgres:10.20
+docker push cimg/postgres:10.20-postgis
 
-docker push cimg/postgres:12.10
-docker push cimg/postgres:12.10-postgis
+docker push cimg/postgres:11.15
+docker push cimg/postgres:11.15-postgis
+
+docker push cimg/postgres:13.6
+docker push cimg/postgres:13.6-postgis
+
+docker push cimg/postgres:14.2
+docker push cimg/postgres:14.2-postgis
