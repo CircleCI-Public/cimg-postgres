674b058e5ca1005d3030732b0a6430e3499f6b74
David Wagner <dwagner@evidation.com>
2022-08-15 22:37:45 -0700

    releases with patch for vulnerability CVE-2022-2625

--- push-images.sh
+++ push-images.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/postgres:10.21
-docker push cimg/postgres:10.21-postgis
+docker push cimg/postgres:10.22
+docker push cimg/postgres:10.22-postgis
 
-docker push cimg/postgres:11.16
-docker push cimg/postgres:11.16-postgis
+docker push cimg/postgres:11.17
+docker push cimg/postgres:11.17-postgis
 
-docker push cimg/postgres:12.11
-docker push cimg/postgres:12.11-postgis
+docker push cimg/postgres:12.12
+docker push cimg/postgres:12.12-postgis
 
-docker push cimg/postgres:13.7
-docker push cimg/postgres:13.7-postgis
+docker push cimg/postgres:13.8
+docker push cimg/postgres:13.8-postgis
 
-docker push cimg/postgres:14.3
-docker push cimg/postgres:14.3-postgis
-
-docker push cimg/postgres:14.4
-docker push cimg/postgres:14.4-postgis
+docker push cimg/postgres:14.5
+docker push cimg/postgres:14.5-postgis
