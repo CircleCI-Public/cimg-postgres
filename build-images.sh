093f6970157e0518ada02f05456366928eefff33
David Wagner <dwagner@evidation.com>
2022-08-16 22:02:56 -0700

    Pub: 10.22,11.17,12.12,13.8, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.21/Dockerfile -t cimg/postgres:10.21 .
-docker build --file 10.21/postgis/Dockerfile -t cimg/postgres:10.21-postgis .
-docker build --file 11.16/Dockerfile -t cimg/postgres:11.16 .
-docker build --file 11.16/postgis/Dockerfile -t cimg/postgres:11.16-postgis .
-docker build --file 12.11/Dockerfile -t cimg/postgres:12.11 .
-docker build --file 12.11/postgis/Dockerfile -t cimg/postgres:12.11-postgis .
-docker build --file 13.7/Dockerfile -t cimg/postgres:13.7 .
-docker build --file 13.7/postgis/Dockerfile -t cimg/postgres:13.7-postgis .
-docker build --file 14.3/Dockerfile -t cimg/postgres:14.3 .
-docker build --file 14.3/postgis/Dockerfile -t cimg/postgres:14.3-postgis .
-docker build --file 14.4/Dockerfile -t cimg/postgres:14.4 .
-docker build --file 14.4/postgis/Dockerfile -t cimg/postgres:14.4-postgis .
+docker build --file 10.22/Dockerfile -t cimg/postgres:10.22 .
+docker build --file 10.22/postgis/Dockerfile -t cimg/postgres:10.22-postgis .
+docker build --file 11.17/Dockerfile -t cimg/postgres:11.17 .
+docker build --file 11.17/postgis/Dockerfile -t cimg/postgres:11.17-postgis .
+docker build --file 12.12/Dockerfile -t cimg/postgres:12.12 .
+docker build --file 12.12/postgis/Dockerfile -t cimg/postgres:12.12-postgis .
+docker build --file 13.8/Dockerfile -t cimg/postgres:13.8 .
+docker build --file 13.8/postgis/Dockerfile -t cimg/postgres:13.8-postgis .
+docker build --file 14.5/Dockerfile -t cimg/postgres:14.5 .
+docker build --file 14.5/postgis/Dockerfile -t cimg/postgres:14.5-postgis .
