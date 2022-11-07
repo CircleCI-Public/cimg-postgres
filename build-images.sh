2af6b253db85da35a99bde6d5507b7af38db099f
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-11-07 15:46:49 -0500

    Publish v12.0 and v15.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.22/Dockerfile -t cimg/postgres:10.22 .
-docker build --file 10.22/postgis/Dockerfile -t cimg/postgres:10.22-postgis .
-docker build --file 11.17/Dockerfile -t cimg/postgres:11.17 .
-docker build --file 11.17/postgis/Dockerfile -t cimg/postgres:11.17-postgis .
-docker build --file 12.12/Dockerfile -t cimg/postgres:12.12 .
-docker build --file 12.12/postgis/Dockerfile -t cimg/postgres:12.12-postgis .
-docker build --file 13.8/Dockerfile -t cimg/postgres:13.8 .
-docker build --file 13.8/postgis/Dockerfile -t cimg/postgres:13.8-postgis .
-docker build --file 14.5/Dockerfile -t cimg/postgres:14.5 .
-docker build --file 14.5/postgis/Dockerfile -t cimg/postgres:14.5-postgis .
+docker build --file 12.0/Dockerfile -t cimg/postgres:12.0 -t cimg/postgres:12.0 .
+docker build --file 12.0/postgis/Dockerfile -t cimg/postgres:12.0-postgis -t cimg/postgres:12.0-postgis .
+docker build --file 15.0/Dockerfile -t cimg/postgres:15.0 -t cimg/postgres:15.0 .
+docker build --file 15.0/postgis/Dockerfile -t cimg/postgres:15.0-postgis -t cimg/postgres:15.0-postgis .
