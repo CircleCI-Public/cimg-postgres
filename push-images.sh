27e24bf1232f8aada576cc3865c2a98d92068f36
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-12-19 19:26:34 -0500

    Publish v13.9, v14.6, and v15.1. [release] (#63)

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/postgres:12.0
-docker push cimg/postgres:12.0-postgis
-docker push cimg/postgres:15.0
-docker push cimg/postgres:15.0-postgis
+docker push cimg/postgres:13.9
+docker push cimg/postgres:13.9-postgis
+docker push cimg/postgres:14.6
+docker push cimg/postgres:14.6-postgis
+docker push cimg/postgres:15.1
+docker push cimg/postgres:15.1-postgis
