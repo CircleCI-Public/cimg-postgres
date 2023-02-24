1b8355315acc2299b2fdb2a31b3de0aba71f5f1d
Jeff Chen <jeff.chen@circleci.com>
2023-02-23 16:01:31 -0800

    testing: build with orb

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 13.9/Dockerfile -t cimg/postgres:13.9 -t cimg/postgres:13.9 .
-docker build --file 13.9/postgis/Dockerfile -t cimg/postgres:13.9-postgis -t cimg/postgres:13.9-postgis .
-docker build --file 14.6/Dockerfile -t cimg/postgres:14.6 -t cimg/postgres:14.6 .
-docker build --file 14.6/postgis/Dockerfile -t cimg/postgres:14.6-postgis -t cimg/postgres:14.6-postgis .
-docker build --file 15.1/Dockerfile -t cimg/postgres:15.1 -t cimg/postgres:15.1 .
-docker build --file 15.1/postgis/Dockerfile -t cimg/postgres:15.1-postgis -t cimg/postgres:15.1-postgis .
+docker build --file 15.1/Dockerfile -t cimg/postgres:15.1 -t cimg/postgres:15.1 --platform linux/amd64 .
+docker build --file 15.1/postgis/Dockerfile -t cimg/postgres:15.1-postgis -t cimg/postgres:15.1-postgis --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/postgres:13.9
-docker push cimg/postgres:13.9-postgis
-docker push cimg/postgres:14.6
-docker push cimg/postgres:14.6-postgis
 docker push cimg/postgres:15.1
 docker push cimg/postgres:15.1-postgis
