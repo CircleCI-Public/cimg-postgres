2f53a8b8c1b1cf235a7f99e64861ac14f1dd533e
mkusaka <hinoshita1992@gmail.com>
2023-03-16 01:01:06 +0900

    Pub: 11.19,12.14,13.10,14.7, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 13.9/Dockerfile -t cimg/postgres:13.9 -t cimg/postgres:13.9 .
-docker build --file 13.9/postgis/Dockerfile -t cimg/postgres:13.9-postgis -t cimg/postgres:13.9-postgis .
-docker build --file 14.6/Dockerfile -t cimg/postgres:14.6 -t cimg/postgres:14.6 .
-docker build --file 14.6/postgis/Dockerfile -t cimg/postgres:14.6-postgis -t cimg/postgres:14.6-postgis .
-docker build --file 15.1/Dockerfile -t cimg/postgres:15.1 -t cimg/postgres:15.1 .
-docker build --file 15.1/postgis/Dockerfile -t cimg/postgres:15.1-postgis -t cimg/postgres:15.1-postgis .
+docker build --file 11.19/Dockerfile -t cimg/postgres:11.19 -t cimg/postgres:11.19 --platform linux/amd64 .
+docker build --file 11.19/postgis/Dockerfile -t cimg/postgres:11.19-postgis -t cimg/postgres:11.19-postgis --platform linux/amd64 .
+docker build --file 12.14/Dockerfile -t cimg/postgres:12.14 -t cimg/postgres:12.14 --platform linux/amd64 .
+docker build --file 12.14/postgis/Dockerfile -t cimg/postgres:12.14-postgis -t cimg/postgres:12.14-postgis --platform linux/amd64 .
+docker build --file 13.10/Dockerfile -t cimg/postgres:13.10 -t cimg/postgres:13.10 --platform linux/amd64 .
+docker build --file 13.10/postgis/Dockerfile -t cimg/postgres:13.10-postgis -t cimg/postgres:13.10-postgis --platform linux/amd64 .
+docker build --file 14.7/Dockerfile -t cimg/postgres:14.7 -t cimg/postgres:14.7 --platform linux/amd64 .
+docker build --file 14.7/postgis/Dockerfile -t cimg/postgres:14.7-postgis -t cimg/postgres:14.7-postgis --platform linux/amd64 .
+docker build --file 15.2/Dockerfile -t cimg/postgres:15.2 -t cimg/postgres:15.2 --platform linux/amd64 .
+docker build --file 15.2/postgis/Dockerfile -t cimg/postgres:15.2-postgis -t cimg/postgres:15.2-postgis --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/postgres:13.9
-docker push cimg/postgres:13.9-postgis
-docker push cimg/postgres:14.6
-docker push cimg/postgres:14.6-postgis
-docker push cimg/postgres:15.1
-docker push cimg/postgres:15.1-postgis
+docker push cimg/postgres:11.19
+docker push cimg/postgres:11.19-postgis
+docker push cimg/postgres:12.14
+docker push cimg/postgres:12.14-postgis
+docker push cimg/postgres:13.10
+docker push cimg/postgres:13.10-postgis
+docker push cimg/postgres:14.7
+docker push cimg/postgres:14.7-postgis
+docker push cimg/postgres:15.2
+docker push cimg/postgres:15.2-postgis
