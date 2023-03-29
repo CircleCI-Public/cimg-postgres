9d42e7fdafbc9a2b8f8d2a0848785b904f6a3b75
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-03-29 13:11:33 -0400

    Pre-tune PostgreSQL for CircleCI & test with 15.2 [release] (#67)

--- push-images.sh
+++ push-images.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/postgres:11.19
-docker push cimg/postgres:11.19-postgis
-docker push cimg/postgres:12.14
-docker push cimg/postgres:12.14-postgis
-docker push cimg/postgres:13.10
-docker push cimg/postgres:13.10-postgis
-docker push cimg/postgres:14.7
-docker push cimg/postgres:14.7-postgis
 docker push cimg/postgres:15.2
 docker push cimg/postgres:15.2-postgis
